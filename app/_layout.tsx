1b2b714fd4f62db8bcf4cda9c314b5d615e9b95f
Michael Medvedev <mmedvedev20@gmail.com>
2026-02-15 14:38:36 -0600

    installed and setup initial dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://744b6c854264b6261aac5aa75d208a51@o4510432959070208.ingest.us.sentry.io/4510891705171968',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   return <Stack />;
-}
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
-import { Text, View } from "react-native";
+import * as Sentry from "@sentry/react-native";
+import { defaultConfig } from "@tamagui/config/v5";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Button, createTamagui, TamaguiProvider, Text, View } from "tamagui";
+
+const config = createTamagui(defaultConfig);
+
+const queryClient = new QueryClient();
+
+const publishableKey = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    // <StripeProvider
+    //   publishableKey={publishableKey!}
+    //   merchantIdentifier="merchant.identifier" // required for Apple Pay
+    //   urlScheme="lockinfrontend" // required for 3D Secure and bank redirects
+    // >
+    <QueryClientProvider client={queryClient}>
+      <TamaguiProvider config={config} defaultTheme="light">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+          <Button
+            onPress={() => {
+              Sentry.captureException(new Error("First error"));
+            }}
+          >
+            Try!
+          </Button>
+        </View>
+      </TamaguiProvider>
+    </QueryClientProvider>
+    // </StripeProvider>
   );
 }
